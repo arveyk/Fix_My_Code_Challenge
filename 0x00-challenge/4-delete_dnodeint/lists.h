5e2d80eca13bf7acd8dd3e88ac5f69209446d2f2
Harvey W. Kis <harveykisiangani@gmail.com>
2023-10-24 15:13:25 +0300

    betty stylw

--- 0x00-challenge/4-delete_dnodeint/lists.h
+++ 0x00-challenge/4-delete_dnodeint/lists.h
@@ -23,5 +23,4 @@ size_t print_dlistint(const dlistint_t *h);
 dlistint_t *add_dnodeint_end(dlistint_t **head, const int n);
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index);
 void free_dlistint(dlistint_t *head);
-
 #endif
