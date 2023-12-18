9d3211a9b0ef3dbb984af0b0eb9ffbb0bd2c37bd
Harvey W. Kis <harveykisiangani@gmail.com>
2023-12-18 12:43:20 +0300

    user.com

--- 0x01-challenge/user.py
+++ 0x01-challenge/user.py
@@ -8,23 +8,22 @@ class User():
 
     def __init__(self):
         """ Documentation """
-        self.__email = None
+        self.email = ""
 
+    @property
+    def email(self):
+        """ Documentation """
+        return self.email
+   
     @email.setter
     def email(self, value):
         """ Documentation """
         if type(value) is not str:
             raise TypeError("email must be a string")
-        self.__email = value
-
-    @property
-    def email(self):
-        """ Documentation """
-        return self.__email
-   
+        self.email = value
     
 if __name__ == "__main__":
 
-    u = User()
+    u = User
     u.email = "john@snow.com"
     print(u.email)
